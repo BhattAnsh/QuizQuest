9e0335ad1987bbd5487336028562069712f42107
BhattAnsh <vimalbhatt906@gmail.com>
2024-03-21 17:39:14 +0530

    removed carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,12 @@ import * as React from 'react';
 import Nav from './navbar/nav';
 import Hero from './heroPage/hero';
 import Landing from './landingPage/landing';
-import Bento from './Bento/Bento';
 function Home() {
     return (
         <div>
           <Nav></Nav>  
           <Hero></Hero>
           <Landing></Landing>
-          <Bento></Bento>
         </div>
     )
 }
